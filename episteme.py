4cbb50ae044ebdc2672fba88cacf581976a10878
olyverha <126532573+olyverha@users.noreply.github.com>
2023-03-02 15:30:47 -0300

    1ª sumarização.

--- episteme.py
+++ episteme.py
@@ -1,12 +1,11 @@
 import openai
-import os
 from time import time, sleep
 import textwrap
 import re
 
 
 def open_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as infile:
+    with open(filepath, 'r', encoding='UTF-8') as infile:
         return infile.read()
 
 
@@ -14,18 +13,19 @@ openai.api_key = open_file('openaiapikey.txt')
 
 
 def save_file(content, filepath):
-    with open(filepath, 'w', encoding='utf-8') as outfile:
+    with open(filepath, 'w', encoding='UTF-8') as outfile:
         outfile.write(content)
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
+def gpt3_completion(_prompt, engine='text-davinci-003', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0,
+                    stop=['<<END>>']):
     max_retry = 5
     retry = 0
     while True:
         try:
             response = openai.Completion.create(
                 engine=engine,
-                prompt=prompt,
+                prompt=_prompt,
                 temperature=temp,
                 max_tokens=tokens,
                 top_p=top_p,
@@ -33,16 +33,16 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.6, top_p=1.0, toke
                 presence_penalty=pres_pen,
                 stop=stop)
             text = response['choices'][0]['text'].strip()
-            text = re.sub('\s+', ' ', text)
+            text = re.sub('\\s+', ' ', text)
             filename = '%s_gpt3.txt' % time()
             with open('gpt3_logs/%s' % filename, 'w') as outfile:
-                outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
+                outfile.write('PROMPT:\n\n' + _prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
             return text
         except Exception as oops:
             retry += 1
             if retry >= max_retry:
                 return "GPT3 error: %s" % oops
-            print('Error communicating with OpenAI:', oops)
+            print('Erro de comunicação com a API da OpenAI:', oops)
             sleep(1)
 
 
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     for chunk in chunks:
         count = count + 1
         prompt = open_file('prompt.txt').replace('<<SUMMARY>>', chunk)
-        prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
+        prompt = prompt.encode(encoding='UTF-8', errors='ignore').decode('UTF-8')
         summary = gpt3_completion(prompt)
         print('\n\n\n', count, 'of', len(chunks), ' - ', summary)
         result.append(summary)
