5467e470dd41c715ff6033014086d85c3744cfc0
olyverha <126532573+olyverha@users.noreply.github.com>
2023-03-15 23:24:38 -0300

    acrescentado prompt TADA.

--- chromedriverChatGPT/pyChatGPT.py
+++ chromedriverChatGPT/pyChatGPT.py
@@ -176,7 +176,7 @@ class ChatGPT:
         self.logger.debug('Initializing browser...')
         options = uc.ChromeOptions()
         options.add_argument('--window-size=1024,768')
-        options.add_argument('--headless')
+        #  options.add_argument('--headless')
         if self.__proxy:
             options.add_argument(f'--proxy-server={self.__proxy}')
         for arg in self.__chrome_args:
@@ -430,7 +430,7 @@ class ChatGPT:
             return print()
 
         self.logger.debug('Waiting for completion...')
-        WebDriverWait(self.driver, 150).until_not(
+        WebDriverWait(self.driver, 600).until_not(
             EC.presence_of_element_located(chatgpt_streaming)
         )
 
@@ -446,7 +446,7 @@ class ChatGPT:
         if len(self.driver.find_elements(*chatgpt_erro_advise)) != 0:  # verifica se há alguma mensagem de erro
             erro = self.driver.find_elements(*chatgpt_erro_advise)[-1]
             erro_tratado = markdownify(erro.get_attribute('innerHTML')).replace('Copy code`', '`')
-            self.logger.debug('Response is an error')
+            self.logger.debug('Erro com caixa de dialogo vermelho')
             raise ValueError(erro_tratado)
 
         response = self.driver.find_elements(*chatgpt_small_response)[-1]
--- episteme.py
+++ episteme.py
@@ -8,7 +8,6 @@ import re
 import PyPDF2
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-from fpdf import FPDF
 
 
 def clear_screen():
@@ -37,24 +36,27 @@ if __name__ == '__main__':
         for pageNum in range(len(pdf.pages)):
             pageObj = pdf.pages[pageNum]
             alltext += pageObj.extract_text()
-    # save_file(alltext, 'output_%s.txt' % time())
-    # alltext = open_file('input.txt')
     alltext = re.sub('\\s+', ' ', alltext)
     chunks = textwrap.wrap(alltext, 2000)
     result = list()
     result_analise = list()
     while True:
-        token = open_file('token.txt')
+        token = open_file('token2.txt')
         session_token = token.replace('\n', '')
         conversation_id = ''
         chat = ChatGPT(session_token, conversation_id)
         break
     clear_screen()
     print(
-        'Iniciando o resumo do documento...\n'
+        'Iniciando conexão com o modelo...\n'
     )
+    time.sleep(10)  # tempo necessário para corrigir o erro de session expired
+
 
     def algoritimo_summary(cont):
+        print(
+            'Iniciando o resumo do documento...\n'
+        )
         contador_geral = cont
         event = threading.Event()
         contador_interno = 0
@@ -66,6 +68,7 @@ if __name__ == '__main__':
 
         for chunk in chunks:
             if contador_geral > contador_interno:
+                print('Salto do contador...')
                 contador_interno += 1
             else:
                 prompt = open_file('prompt.txt').replace('<<SUMMARY>>', chunk)
@@ -76,21 +79,27 @@ if __name__ == '__main__':
                 text = summary['message'].strip()  # pega somente o conteúdo da mensagem sem o conversation_id
                 text = re.sub('\\s+', ' ', text)  # substitui todas as ocorrências de um ou mais espaços em branco por
                 #                                   um único espaço em branco
-            #   filename = '%s_gpt3.txt' % time()
-            #   with open('gpt3_logs/%s' % filename, 'w') as outfile:
-            #       outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
+                #   filename = '%s_gpt3.txt' % time()
+                #   with open('gpt3_logs/%s' % filename, 'w') as outfile:
+                #       outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
                 print('\n\n\n', contador_geral + 1, 'of', len(chunks), ' - ', text)
                 result.append(text)
                 contador_geral += 1
                 contador_interno += 1
 
     #  Cria a análise baseada no resumo
-    def algoritimo_analisis(summary, questions):
+    def algoritimo_analisis(summary, questions, char):
+        print(
+            'Iniciando a análise do documento...\n'
+        )
         event = threading.Event()
         count = 0
         resumo = summary
         resumo = re.sub('\\s+', ' ', resumo)
-        perguntas = questions
+        pedacos = resumo
+        if char > 0:  # caso o resumo seja muito grande, precisa ser informado a quantidade de caracteres por analise
+            pedacos = textwrap.wrap(resumo, char)
+        perguntas = re.sub('\\s+', ' ', questions)
         sentences = perguntas.split('<<NEW QUESTION>>')
 
         def consulta_analise(_prompt):
@@ -98,69 +107,94 @@ if __name__ == '__main__':
             event.set()  # envia o sinal para o event.wait() continuar
             return resultado
 
-        for sentence in sentences:
-            prompt = open_file('prompt_analise.txt').replace('<<QUESTION>>', sentence)
-            prompt = prompt.replace('<<SUMMARY>>', resumo)
-            prompt = prompt.encode(encoding='UTF-8', errors='ignore').decode('UTF-8')
-            summary = consulta_analise(prompt)
-            event.wait()  # aguarda o sinal para continuar
-            event.clear()  # limpa o sinal para a próxima iteração
-            text = summary['message'].strip()  # pega somente o conteúdo da mensagem sem o conversation_id
-            text = re.sub('\\s+', ' ', text)  # substitui todas as ocorrências de um ou mais espaços em branco por
-            #                                   um único espaço em branco
-            #   filename = '%s_gpt3.txt' % time()
-            #   with open('gpt3_logs/%s' % filename, 'w') as outfile:
-            #       outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
-            print('\n\n\n', count + 1, 'of', len(sentences), ' - ', text)
-            result_analise.append(sentence + ':' + '\n' + text)
-            count += 1
+        if char > 0:
+            cont = 1
+            for pedaco in pedacos:
+                for sentence in sentences:
+                    prompt = open_file('prompt_analise.txt').replace('<<QUESTION>>', sentence)
+                    prompt = prompt.replace('<<SUMMARY>>', pedaco)
+                    prompt = prompt.encode(encoding='UTF-8', errors='ignore').decode('UTF-8')
+                    summary = consulta_analise(prompt)
+                    event.wait()  # aguarda o sinal para continuar
+                    event.clear()  # limpa o sinal para a próxima iteração
+                    text = summary['message'].strip()  # pega somente o conteúdo da mensagem sem o conversation_id
+                    text = re.sub('\\s+', ' ', text)  # substitui todas as ocorrências de um ou mais espaços em branco
+                    #                                   por um único espaço em branco
+                    #   filename = '%s_gpt3.txt' % time()
+                    #   with open('gpt3_logs/%s' % filename, 'w') as outfile:
+                    #       outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
+                    print('\n\n\n', count + 1, 'of', len(sentences), ' - ', text)
+                    result_analise.append(sentence + '\n' + ':' + '\n' + text)
+                    count += 1
+            cont += 1
+        else:
+            print('realizando analise simples...')
+            for sentence in sentences:
+                prompt = open_file('prompt_analise.txt').replace('<<QUESTION>>', sentence)
+                prompt = prompt.replace('<<SUMMARY>>', resumo)
+                prompt = prompt.encode(encoding='UTF-8', errors='ignore').decode('UTF-8')
+                summary = consulta_analise(prompt)
+                event.wait()  # aguarda o sinal para continuar
+                event.clear()  # limpa o sinal para a próxima iteração
+                text = summary['message'].strip()  # pega somente o conteúdo da mensagem sem o conversation_id
+                text = re.sub('\\s+', ' ', text)  # substitui todas as ocorrências de um ou mais espaços em branco por
+                #                                   um único espaço em branco
+                #   filename = '%s_gpt3.txt' % time()
+                #   with open('gpt3_logs/%s' % filename, 'w') as outfile:
+                #       outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
+                print('\n\n\n', count + 1, 'of', len(sentences), ' - ', text)
+                result_analise.append(sentence + '\n' + ':' + '\n' + text)
+                count += 1
 
-
-    #  Executa o programa
-    try:
-        algoritimo_summary(0)
-        tempo_atual = time.time()
-        data_hora_formatada = time.strftime('%d%m%Y_%H%M%S', time.localtime(tempo_atual))
-
-        # data_hora_formatada = '13032023_002247'  # apagar
-
+    def processa_resumo(data_hora_formatada, pos_inicial_resumo):
+        algoritimo_summary(pos_inicial_resumo)  # apontar o último pedaço resumido de maneira a recomeçar pelo seguinte
         titulo_resumo = tituloDoc + '_RESUMO_' + '[' + data_hora_formatada + ']' + '.txt'
         save_file('\n\n'.join(result), 'repositorio/' + titulo_resumo)
         time.sleep(2)  # aguarda salvar o arquivo em disco
-        print(
-            'Iniciando a análise do documento...\n'
-        )
-        algoritimo_analisis(open_file('repositorio/' + titulo_resumo), open_file('questions.txt'))
+
+    def processa_analise(data_hora_formatada, num_carcteres):
+        titulo_resumo = tituloDoc + '_RESUMO_' + '[' + data_hora_formatada + ']' + '.txt'
+        algoritimo_analisis(open_file('repositorio/' + titulo_resumo), open_file('questions.txt'), num_carcteres)
         titulo_analise = tituloDoc + '_ANALISE_' + '[' + data_hora_formatada + ']' + '.txt'
         save_file('\n\n'.join(result_analise), 'repositorio/' + titulo_analise)
 
+    def unir_resumo_analise(data_hora_formatada):
+        titulo_resumo = tituloDoc + '_RESUMO_' + '[' + data_hora_formatada + ']' + '.txt'
+        titulo_analise = tituloDoc + '_ANALISE_' + '[' + data_hora_formatada + ']' + '.txt'
         with open('repositorio/' + titulo_resumo, 'r', encoding='utf-8') as f1, open(
                 'repositorio/' + titulo_analise, 'r', encoding='utf-8') as f2:
             conteudo_arquivo1 = f1.read()
             conteudo_arquivo2 = f2.read()
 
         with open('repositorio/' + tituloDoc + '_RESUMO_ANALISE_' + '.txt', 'w') as f3:
-            f3.write('RESUMO_' + tituloDoc + '[' + data_hora_formatada + ']' + '\n' + conteudo_arquivo1 + '\n\n\n')
-            f3.write('ANALISE_' + tituloDoc + '[' + data_hora_formatada + ']' + '\n' + conteudo_arquivo2)
+            f3.write('RESUMO_' + tituloDoc + '[' + data_hora_formatada + ']' + '\n\n' + conteudo_arquivo1 + '\n\n\n')
+            f3.write('ANALISE_' + tituloDoc + '[' + data_hora_formatada + ']' + '\n\n' + conteudo_arquivo2)
 
-        """
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_title('RESUMO E ANÁLISE DE ' + tituloDoc)
+    def processamento_completo(pos_inicial_resumo, num_carcteres_analise):
+        tempo_atual = time.time()
+        data_hora_formatada = time.strftime('%d%m%Y_%H%M%S', time.localtime(tempo_atual))
+        processa_resumo(data_hora_formatada, pos_inicial_resumo)
+        processa_analise(data_hora_formatada, num_carcteres_analise)
+        unir_resumo_analise(data_hora_formatada)
 
-        pdf.set_font('Arial', '', 12)
+    def processamento_1etapa(pos_inicial_resumo):
+        tempo_atual = time.time()
+        data_hora_formatada = time.strftime('%d%m%Y_%H%M%S', time.localtime(tempo_atual))
+        processa_resumo(data_hora_formatada, pos_inicial_resumo)
 
-        with open('repositorio/' + titulo_resumo, 'r', encoding='utf-8') as f:
-            text = f.read()
-            pdf.cell(0, 10, text, ln=True)
+    def processamento_2etapa(data_hora_formatada, num_carcteres):
+        processa_analise(data_hora_formatada, num_carcteres)
+        unir_resumo_analise(data_hora_formatada)
 
-        with open('repositorio/' + titulo_analise, 'r', encoding='utf-8') as f:
-            text = f.read()
-            pdf.cell(0, 10, text)
+    def processamento_3etapa(data_hora_formatada):
+        unir_resumo_analise(data_hora_formatada)
 
-        nomePDF = 'repositorio/RESUMO_ANALISE_' + tituloDoc + '.pdf'
-        pdf.output(nomePDF)
-        """
+    #  Executa o programa
+    try:
+        processamento_completo(0, 0)
+        #  processamento_1etapa(15)
+        #  processamento_2etapa('14032023_171735', 0)
+        #  processamento_3etapa(data_hora_formatada)
 
     except seleniumexceptions.TimeoutException:
         print('.Limite de tempo de espera da requisição atingido. Execute novamente o programa.')
